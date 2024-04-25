0d65f3250410ef637276b73fcaddb207221922bd
Patricio Cortes <patricio.cortes@noirlab.edu>
2024-04-25 11:04:47 -0400

    Update code after Te-Wei's comments.

--- python/publish_telemetry.py
+++ python/publish_telemetry.py
@@ -15,16 +15,10 @@ localhost = "127.0.0.1"
 parser = argparse.ArgumentParser()
 parser.add_argument("port_cmd", type=int, metavar="pcmd", help="set the command port")
 parser.add_argument("port_tel", type=int, metavar="ptel", help="set the telemetry port")
-parser.add_argument("--ncmd", type=int, metavar="N", default=10, help="set the number of commands to be sent (default 10)")
-#parser.add_argument("--frec", type=int, metavar="F", default=10, help="set the frecuency in Hz of the telemetry to be published (default 10 Hz)")
-#parser.add_argument("--dur", type=int, metavar="D", default=2, help="set the duration in seconds of the telemetry to be published (default 2 seconds)")
 args = parser.parse_args()
 port_cmd = args.port_cmd
 port_tel = args.port_tel
-num_cmd = args.ncmd
-#frequency = args.frec
 frequency = 10
-#duration = args.dur
 
 # define the main
 async def main(duration):
@@ -43,7 +37,6 @@ async def main(duration):
     print(f'Duration = {duration} seconds.')
     tel_details = {"state": 2, "compName": "MTMount"} 
     tcp_client_tel.register_tel("elevation", tel_details) 
-    #tcp_client_tel.register_tel("azimuth", tel_details) 
     await tcp_client_tel.pub_tel_continuous(frequency, duration) 
     await asyncio.sleep(2)
 
@@ -58,7 +51,4 @@ async def main(duration):
 asyncio.run(main(2))
 time.sleep(2)
 asyncio.run(main(10))
-#time.sleep(2)
-#asyncio.run(main(20))
-#time.sleep(3)
-#asyncio.run(main(30))
+time.sleep(2)
